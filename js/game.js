7fffc23b3e3b5b1ae913e5f1f67470f61d8e680c
Nick Jones <nickjones@toast.local>
2017-12-22 10:10:53 -0500

    Changing colors! - Users can now select one of four shades of gray to draw with - Adds one additional ninja

--- js/game.js
+++ js/game.js
@@ -1,27 +1,37 @@
 $(document).ready (function() {
+  color1 = 'filledBlack'
+  color2 = 'filledGray'
+  color3 = 'filledLightGray'
+  color4 = 'filledLightestGray'
+
   // setup squares
   var nodeCount = 576
   for (var i = 1; i <= nodeCount; i++) {
     $('.canvas').append('<div class="node"></div>');
   }
 
-  // handle ink colors
-
   // reset board
   function resetBoard () {
     var answer = confirm('Are you sure you want to reset?')
       if (answer) {
         console.log('User reset board')
-        $('.node').removeClass('filledBlack')
+        $('.node').removeClass(color1).removeClass(color2).removeClass(color3).removeClass(color4)
       } else {
         console.log('User declined reset')
       }
   }
 
   function draw () {
-
-    $('.node').bind('click', function (event) {
-      $(this).toggleClass('filledBlack')
+    $('.node').click(function () {
+      if($('#black').is(':checked')) {
+        $(this).toggleClass(color1)
+      } else if ($('#gray').is(':checked')) {
+        $(this).toggleClass(color2)
+      } else if ($('#light-gray').is(':checked')) {
+        $(this).toggleClass(color3)
+      } else if ($('#lightest-gray').is(':checked')) {
+        $(this).toggleClass(color4)
+      }
     })
   }
 
