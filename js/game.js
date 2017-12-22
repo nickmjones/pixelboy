4eeffe7aa2ab4cf7201ebbdc838531c4b91408f5
Nick Jones <nickjones@toast.local>
2017-12-21 21:34:15 -0500

    Pausing

--- js/game.js
+++ js/game.js
@@ -1,34 +1,33 @@
-$(document).ready(function(){
+$(document).ready (function() {
   // setup squares
-  var nodeCount = 576;
+  var nodeCount = 576
   for (var i = 1; i <= nodeCount; i++) {
     $('.canvas').append('<div class="node"></div>');
   }
 
+  // handle ink colors
+
   // reset board
-  function resetBoard() {
-    var answer = confirm('Are you sure you want to reset?');
-      if (answer)
-      {
-        console.log('user reset board');
-        $('.node').removeClass('filled')
-      }
-      else
-      {
-        console.log('user declined reset');
+  function resetBoard () {
+    var answer = confirm('Are you sure you want to reset?')
+      if (answer) {
+        console.log('User reset board')
+        $('.node').removeClass('filledBlack')
+      } else {
+        console.log('User declined reset')
       }
   }
 
-  function draw() {
+  function draw () {
 
-    $('.node').bind("click", function(event) {
-      $(this).toggleClass('filled');
-    });
+    $('.node').bind('click', function (event) {
+      $(this).toggleClass('filledBlack')
+    })
   }
 
   draw()
 
-  $('#reset').click (function() {
+  $('#reset').click(function () {
     resetBoard()
-  });
-});
+  })
+})
